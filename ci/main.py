43342486732dde9bba57948874839ebe33492c75
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-07-24 11:21:21 -0300

    Add SP metric

--- ci/main.py
+++ ci/main.py
@@ -4,11 +4,14 @@
 
 from test_import import test_import
 from test_callbacks import test_sp_callback
+from test_sp_metric import test as test_metric
 
 
 if __name__ == '__main__':
     print (" ===> BEGIN CI TEST <=== ")
     print (" ==> Import saphyra test")
     test_import()
-    print (" ==> Training a basic model with SP callback")
+    # print (" ==> Training a basic model with SP callback")
     #test_sp_callback()
+    print (" ==> Testing SP metric")
+    test_metric()
