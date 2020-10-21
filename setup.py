90fa9d7dbdc2be7f8eef06b085e107d14e760449
jodafons <jodafons@lps.ufrj.br>
2020-10-20 21:49:52 -0300

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'saphyra',
-  version = '3.0.8',
+  version = '3.0.9',
   license='GPL-3.0',
   description = '',
   long_description = long_description,
