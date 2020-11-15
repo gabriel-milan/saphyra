4143b68907bffa7410694c13547b57f00e2689ed
jodafons <jodafons@lps.ufrj.br>
2020-11-15 16:54:49 -0300

    fix import

--- saphyra/utils/plot_generator.py
+++ saphyra/utils/plot_generator.py
@@ -1,6 +1,8 @@
+__all__ = ['plot_training_curves']
 
 def plot_training_curves( context ):
 
+    import matplotlib.pyplot as plt
     start_epoch=3
     sort = context.getHandler('sort')
     init = context.getHandler('init')
@@ -10,21 +12,21 @@ def plot_training_curves( context ):
     fig, ax = plt.subplots(2,1, figsize=(10,15))
     fig.suptitle(r'Monitoring Train Plot - Sort = %d, Init = %d, Imodel = %d'%(sort,init,imodel), fontsize=15)
     
-    best_epoch = history['max_sp_best_epoch_val'][-1]
+    best_epoch = history['max_sp_best_epoch_val'][-1] - start_epoch
     # Make the plot here
-    ax[0, 0].set_xlabel('Epochs')
-    ax[0, 0].set_ylabel('Loss')
-    ax[0, 0].plot(history['loss'][start_epoch::], c='b', label='Train Step')
-    ax[0, 0].plot(history['val_loss'][start_epoch::], c='r', label='Validation Step') 
-    ax[0, 0].axvline(x=best_epoch, c='k', label='Best epoch')
-    ax[0, 0].legend()
-    ax[0, 0].grid()
-    ax[1, 0].set_xlabel('Epochs')
-    ax[1, 0].set_ylabel('SP'%sort)
-    ax[1, 0].plot(history['max_sp_val'][start_epoch::], c='r', label='Validation Step') 
-    ax[1, 0].axvline(x=best_epoch, c='k', label='Best epoch')
-    ax[1, 0].legend()
-    ax[1, 0].grid()
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('Loss')
+    ax[0].plot(history['loss'][start_epoch::], c='b', label='Train Step')
+    ax[0].plot(history['val_loss'][start_epoch::], c='r', label='Validation Step') 
+    ax[0].axvline(x=best_epoch, c='k', label='Best epoch')
+    ax[0].legend()
+    ax[0].grid()
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('SP')
+    ax[1].plot(history['max_sp_val'][start_epoch::], c='r', label='Validation Step') 
+    ax[1].axvline(x=best_epoch, c='k', label='Best epoch')
+    ax[1].legend()
+    ax[1].grid()
     plt.savefig(output)
     plt.close(fig)
         
